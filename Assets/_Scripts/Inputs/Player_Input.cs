ce0d5b436aa9b5d946d8c76f35006fe6905984f7
Smirnov Ivan <smirnowwwivan@mail.ru>
2023-07-13 01:57:13 +0300

    Изменил инвентарь

--- Assets/_Scripts/Inputs/Player_Input.cs
+++ Assets/_Scripts/Inputs/Player_Input.cs
@@ -1,4 +1,4 @@
-using UnityEngine;
+/*using UnityEngine;
 
 namespace Inputs
 {
@@ -29,4 +29,4 @@ namespace Inputs
                 _player.MovementComponent.IsJumpPressing = false;
                 _player.MovementComponent.JumpButtonWasPressed = false;
             }
-        }  
+        }  */
